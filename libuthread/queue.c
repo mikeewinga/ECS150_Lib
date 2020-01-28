8907c6d532b562fad5d76055dc0cf3a1fd15180c
mikeewinga <59758904+mikeewinga@users.noreply.github.com>
2020-01-28 13:28:37 -0800

    Added "node" struct and implimented create, enqueue, destroy.

--- libuthread/queue.c
+++ libuthread/queue.c
@@ -4,36 +4,58 @@
 
 #include "queue.h"
 
-typedef struct queue Queue;
+typedef struct node node_t;
+
+struct node 
+{
+    void* data;
+    node_t* next;
+    node_t* prev;
+};
 
 struct queue 
 {
-    void* data;
-    Queue* next;
-    Queue* prev;
+    node_t* head;
+    node_t* tail;
+    int num_nodes;
 };
 
 queue_t queue_create(void)
 {
-    Queue HEAD = (Queue*)malloc(sizeof(Queue));
-    HEAD.data = NULL;
-    HEAD.next = NULL;
-    HEAD.prev = NULL;
-    if(!HEAD) {
+    queue_t q=(queue_t*)malloc(sizeof(queue_t));
+    q->head=NULL;
+    q->tail=NULL;
+    q->num_nodes=0;
+    if(!q) {
         return(NULL);
     }
-    return(*HEAD);
-    /* Maybe add a TAIL? */
+    return(*q);
 }
 
 int queue_destroy(queue_t queue)
 {
-    /* TODO Phase 1 */
+    if(queue==NULL || queue->num_nodes!=0){
+        return(-1);
+    }
+    else{
+        free(queue);
+        return(0);
+    }
 }
 
 int queue_enqueue(queue_t queue, void *data)
 {
-	/* TODO Phase 1 */
+    if(queue==NULL || data==NULL){
+        return(-1);
+    }
+    node_t new_node;
+    new_node.data = data;
+    new_node.prev=queue->tail;
+    new_node.next=queue->tail->next;
+    queue->tail->next=new_node;
+    queue->tail=new_node;
+    queue->num_nodes+=1;
+    return(0);
 }
 
 int queue_dequeue(queue_t queue, void **data)
@@ -43,7 +65,13 @@ int queue_dequeue(queue_t queue, void **data)
 
 int queue_delete(queue_t queue, void *data)
 {
-	/* TODO Phase 1 */
+    /* Need to change to work with node_t */
+    node_t* next_node;
+    if (queue->next != NULL) {
+        next_q = queue->next;
+        queue_destroy(next_q);
+    }
+    free(queue);
 }
 
 int queue_iterate(queue_t queue, queue_func_t func, void *arg, void **data)
