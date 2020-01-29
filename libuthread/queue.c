800b70d9a1585ddff253dee3acc755826757757f
mikeewinga <59758904+mikeewinga@users.noreply.github.com>
2020-01-29 00:33:22 -0800

    Added more functions. Needs debugging.

--- libuthread/queue.c
+++ libuthread/queue.c
@@ -6,27 +6,37 @@
 
 typedef struct node node_t;
 
-struct node 
+struct node
 {
     void* data;
     node_t* next;
     node_t* prev;
 };
 
-struct queue 
+struct queue
 {
     node_t* head;
     node_t* tail;
     int num_nodes;
 };
 
+node_t node_create(void* data)
+{
+    node_t* n = (node_t*)malloc(sizeof(node_t));
+    n->data = data;
+    if (n->data != NULL) {
+        return((node_t*)NULL);
+    }
+    return(n);
+}
+
 queue_t queue_create(void)
 {
-    queue_t q=(queue_t*)malloc(sizeof(queue_t));
-    q->head=NULL;
-    q->tail=NULL;
-    q->num_nodes=0;
-    if(!q) {
+    queue_t q = (queue_t)malloc(sizeof(queue_t));
+    q->head = NULL;
+    q->tail = NULL;
+    q->num_nodes = 0;
+    if (!q) {
         return(NULL);
     }
     return(*q);
@@ -34,10 +44,10 @@ queue_t queue_create(void)
 
 int queue_destroy(queue_t queue)
 {
-    if(queue==NULL || queue->num_nodes!=0){
+    if (queue == NULL || queue->num_nodes != 0) {
         return(-1);
     }
-    else{
+    else {
         free(queue);
         return(0);
     }
@@ -45,22 +55,43 @@ int queue_destroy(queue_t queue)
 
 int queue_enqueue(queue_t queue, void *data)
 {
-    if(queue==NULL || data==NULL){
+    if (queue == NULL || data == NULL) {
         return(-1);
     }
-    node_t new_node;
-    new_node.data = data;
-    new_node.prev=queue->tail;
-    new_node.next=queue->tail->next;
-    queue->tail->next=new_node;
-    queue->tail=new_node;
-    queue->num_nodes+=1;
+    node_t new_node = node_create(data);
+    if (queue->num_nodes == 0) {
+        new_node.prev = NULL;
+        new_node.next = NULL;
+        queue->head = new_node;
+        queue->tail = new_node;
+    }
+    else {
+        new_node.prev = queue->tail;
+        new_node.next = queue->tail->next;
+        queue->tail->next = new_node;
+        queue->tail = new_node;
+    }
+    queue->num_nodes += 1;
     return(0);
 }
 
 int queue_dequeue(queue_t queue, void **data)
 {
-	/* TODO Phase 1 */
+    node_t old_node = queue->head;
+    if (queue == NULL || data == NULL || !old_node.data) {
+        return(-1);
+    }
+    data = queue->head.data;
+    if (queue->num_nodes == 1) {
+        queue->head = NULL;
+        queue->tail = NULL;
+    }
+    else {
+        old_node.next.prev = old_node.prev;
+        queue->head = old_node.next;
+    }
+    free(old_node);
+    return(0);
 }
 
 int queue_delete(queue_t queue, void *data)
@@ -76,11 +107,11 @@ int queue_delete(queue_t queue, void *data)
 
 int queue_iterate(queue_t queue, queue_func_t func, void *arg, void **data)
 {
-	/* TODO Phase 1 */
+    /* TODO Phase 1 */
 }
 
 int queue_length(queue_t queue)
 {
-	/* TODO Phase 1 */
+    /* TODO Phase 1 */
 }
 
