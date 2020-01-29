7c7fb73f0b770d2609ba3d390b7a18fe0eeac77c
mikeewinga <59758904+mikeewinga@users.noreply.github.com>
2020-01-29 00:30:39 -0800

    Revert "Added  more functions. Needs debugging"
    This reverts commit 3fcaf62802ac414324756c257cabb76f6a910905.

--- libuthread/queue.c
+++ libuthread/queue.c
@@ -20,19 +20,9 @@ struct queue
     int num_nodes;
 };
 
-node_t node_create(void* data)
-{
-    node_t* n=(node_t*)malloc(sizeof(node_t));
-    n->data = data;
-    if (n->data!=NULL) {
-        return((node_t*)NULL);
-    }
-    return(n);
-}
-
 queue_t queue_create(void)
 {
-    queue_t q=(queue_t)malloc(sizeof(queue_t));
+    queue_t q=(queue_t*)malloc(sizeof(queue_t));
     q->head=NULL;
     q->tail=NULL;
     q->num_nodes=0;
@@ -58,40 +48,19 @@ int queue_enqueue(queue_t queue, void *data)
     if(queue==NULL || data==NULL){
         return(-1);
     }
-    node_t new_node=node_create(data);
-    if(queue->num_nodes==0){
-        new_node.prev = NULL;
-        new_node.next = NULL;
-        queue->head = new_node;
-        queue->tail = new_node;
-    }
-    else{
-        new_node.prev=queue->tail;
-        new_node.next=queue->tail->next;
-        queue->tail->next=new_node;
-        queue->tail=new_node;
-    }
+    node_t new_node;
+    new_node.data = data;
+    new_node.prev=queue->tail;
+    new_node.next=queue->tail->next;
+    queue->tail->next=new_node;
+    queue->tail=new_node;
     queue->num_nodes+=1;
     return(0);
 }
 
 int queue_dequeue(queue_t queue, void **data)
 {
-    node_t old_node=queue->head;
-    if (queue == NULL || data == NULL || !old_node.data) {
-        return(-1);
-    }
-    data = queue->head.data;
-    if(queue->num_nodes==1){
-        queue->head=NULL;
-        queue->tail=NULL;
-    }
-    else{
-        old_node.next.prev=old_node.prev;
-        queue->head=old_node.next;
-    }
-    free(old_node);
-    return(0);
+	/* TODO Phase 1 */
 }
 
 int queue_delete(queue_t queue, void *data)
