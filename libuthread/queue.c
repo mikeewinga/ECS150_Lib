c641e540d10faf2bf67faffe44699277da710658
mikeewinga <59758904+mikeewinga@users.noreply.github.com>
2020-01-29 11:50:39 -0800

    Removed bugs. Still need to impliment delete and iterate.

--- libuthread/queue.c
+++ libuthread/queue.c
@@ -4,7 +4,7 @@
 
 #include "queue.h"
 
-typedef struct node node_t;
+typedef struct node* node_t;
 
 struct node
 {
@@ -22,10 +22,11 @@ struct queue
 
 node_t node_create(void* data)
 {
-    node_t* n = (node_t*)malloc(sizeof(node_t));
+    node_t n = malloc(sizeof(node_t));
     n->data = data;
-    if (n->data != NULL) {
-        return((node_t*)NULL);
+    if (n->data == NULL) {
+        free(n);
+        return(*((node_t*)NULL));
     }
     return(n);
 }
@@ -33,13 +34,14 @@ node_t node_create(void* data)
 queue_t queue_create(void)
 {
     queue_t q = (queue_t)malloc(sizeof(queue_t));
-    q->head = NULL;
-    q->tail = NULL;
+    q->head = ((node_t*)NULL);
+    q->tail = ((node_t*)NULL);
     q->num_nodes = 0;
     if (!q) {
-        return(NULL);
+        free(q);
+        return(*((queue_t*)NULL));
     }
-    return(*q);
+    return(q);
 }
 
 int queue_destroy(queue_t queue)
@@ -60,16 +62,17 @@ int queue_enqueue(queue_t queue, void *data)
     }
     node_t new_node = node_create(data);
     if (queue->num_nodes == 0) {
-        new_node.prev = NULL;
-        new_node.next = NULL;
-        queue->head = new_node;
-        queue->tail = new_node;
+        new_node->prev = ((node_t*)NULL);
+        new_node->next = ((node_t*)NULL);
+        queue->head = &new_node;
+        queue->tail = &new_node;
     }
     else {
-        new_node.prev = queue->tail;
-        new_node.next = queue->tail->next;
-        queue->tail->next = new_node;
-        queue->tail = new_node;
+        node_t tail_node= *queue->tail;
+        new_node->prev = queue->tail;
+        new_node->next = tail_node->next;
+        tail_node->next = &new_node;
+        queue->tail = &new_node;
     }
     queue->num_nodes += 1;
     return(0);
@@ -77,18 +80,19 @@ int queue_enqueue(queue_t queue, void *data)
 
 int queue_dequeue(queue_t queue, void **data)
 {
-    node_t old_node = queue->head;
-    if (queue == NULL || data == NULL || !old_node.data) {
+    node_t old_node = *queue->head;
+    if (queue == NULL || data == NULL || !old_node->data) {
         return(-1);
     }
-    data = queue->head.data;
+    data = &old_node->data;
     if (queue->num_nodes == 1) {
         queue->head = NULL;
         queue->tail = NULL;
     }
     else {
-        old_node.next.prev = old_node.prev;
-        queue->head = old_node.next;
+        node_t next_oldest_node = *old_node->next;
+        next_oldest_node->prev = old_node->prev;
+        queue->head = old_node->next;
     }
     free(old_node);
     return(0);
@@ -96,13 +100,14 @@ int queue_dequeue(queue_t queue, void **data)
 
 int queue_delete(queue_t queue, void *data)
 {
-    /* Need to change to work with node_t */
+    /* Need to change to work with node_t 
     node_t* next_node;
     if (queue->next != NULL) {
         next_q = queue->next;
         queue_destroy(next_q);
     }
     free(queue);
+    */
 }
 
 int queue_iterate(queue_t queue, queue_func_t func, void *arg, void **data)
@@ -112,6 +117,6 @@ int queue_iterate(queue_t queue, queue_func_t func, void *arg, void **data)
 
 int queue_length(queue_t queue)
 {
-    /* TODO Phase 1 */
+    return(&queue->num_nodes);
 }
 
